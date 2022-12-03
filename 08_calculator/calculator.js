ec52f6e6e54ae7871aa239b40e7a26738495781b
Ross <rosshamey@gmail.com>
2022-12-03 13:52:59 -0500

    finish #8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,79 @@
-const add = function() {
-	
-};
+const add = function(num1, num2) {
 
-const subtract = function() {
-	
-};
+  let result = num1 + num2;
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
+  return result;
 
 };
 
-const power = function() {
-	
+const subtract = function(num1, num2) {
+
+  let result = num1 - num2;
+
+  return result;
+
 };
 
-const factorial = function() {
-	
+const sum = function(arr) {
+
+  let runningSum = 0;
+  let i = 0;
+
+  for (i = 0; i < arr.length; i++) {
+    let currentNum = arr[i];
+    runningSum += currentNum;
+  }
+
+  return runningSum;
+
+};
+
+const multiply = function(arr) {
+
+  let product = 1;
+  let i = 0;
+
+  for (i = 0; i < arr.length; i++) {
+    let currentNum = arr[i];
+    product *= currentNum;
+  }
+
+  return product;
+
+};
+
+const power = function(number, power) {
+
+  let i = 0;
+  let product = 1;
+
+  for (i = 0; i < power; i++) {
+    product *= number;
+  }
+
+  return product;
+
+};
+
+const factorial = function(number) {
+
+  if (number === 0) {
+    return 1;
+  }
+
+  if (number === 1) {
+    return 1;
+  }
+
+  let i = 0;
+  let product = 1;
+
+  for (i = 1; i <= number; i++) {
+    product *= i;
+  }
+
+  return product;
+
 };
 
 // Do not edit below this line
