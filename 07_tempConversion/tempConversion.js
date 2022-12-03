c375996978ce296f22306ebd325008f42107a17b
Ross <rosshamey@gmail.com>
2022-12-03 13:40:38 -0500

    finish #7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+
+  let cels = (fahrenheit - 32) * 0.5556;
+  let rounded = Number(cels).toFixed(1);
+  let result = parseFloat(rounded);
+
+  return result;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+
+  let fahr = (celsius * 1.8) + 32;
+  let rounded = Number(fahr).toFixed(1);
+  let result = parseFloat(rounded);
+
+  return result;
+
 };
 
 // Do not edit below this line
