dc5133677f203cad2b8521eb50a59a17f11daf5c
Ross <rosshamey@gmail.com>
2022-12-03 12:35:59 -0500

    1 thru 3 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let i = 0;
+    let newString = string;
 
+    if (num < 0) {
+        return "ERROR";
+    }
+
+    if (num === 0) {
+        return "";
+    }
+
+    for (i = 0; i < num - 1; i++) {
+        newString += string;
+    }
+
+    return newString;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let strings = string.split(" ");
+    let reversedOrder = strings.reverse();
+    let newArr = [];
 
+    for (let i = 0; i < strings.length; i++) {
+        let currentStr = reversedOrder[i];
+        letters = currentStr.split("");
+        let reversedLetters = letters.reverse();
+        let resultStr = reversedLetters.join("");
+        newArr.push(resultStr);
+    }
+
+    let final = newArr.join(" ");
+    return final;
 };
 
 // Do not edit below this line
