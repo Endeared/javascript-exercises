9c7f1fa275e830177ff65b01dc91be440d5ab677
Ross <rosshamey@gmail.com>
2022-12-03 14:15:10 -0500

    finish #9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,34 @@
-const palindromes = function () {
+const palindromes = function (string) {
+
+    let replace =  [" ", "!", ",", "."]
+    let i = 0;
+    let newStr = ""
+
+    for (i = 0; i < string.length; i++) {
+        let currentChar = string[i];
+    if (replace.includes(currentChar)) {
+    } else {
+        newStr += currentChar;
+    }
+    }
+
+    let lowercase = newStr.toLowerCase();
+    let store = lowercase;
+    let toRev = [];
+
+    for (i = 0; i < lowercase.length; i++) {
+        let currentChar = lowercase[i];
+    toRev.push(currentChar);
+    }
+
+    let reversed = toRev.reverse();
+    let result = toRev.join("");
+
+    if (store === result) {
+        return true;
+    }
+
+    return false;
 
 };
 
