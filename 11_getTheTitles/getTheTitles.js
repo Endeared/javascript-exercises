361744440bddf0acbd1b298a4c41b883b00eb091
Ross <rosshamey@gmail.com>
2022-12-03 14:52:38 -0500

    finish #11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+
+    let titleArr = [];
+
+    books.forEach(function(obj) {
+    	titleArr.push(obj.title);
+    })
+
+    return titleArr;
 
 };
 
