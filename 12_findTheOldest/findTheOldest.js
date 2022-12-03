a7dbd289f36a783095f5a1ed13d7f7ee4f7600ad
Ross <rosshamey@gmail.com>
2022-12-03 15:07:15 -0500

    test file not working, but verified ans on fiddle

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    let ageArr = [];
+    let nameArr = [];
+
+    people.forEach(function(obj) {
+        if (obj.yearOfDeath === undefined) {
+            obj.yearOfDeath = 2022;
+        }
+        age = (obj.yearOfDeath - obj.yearOfBirth);
+        ageArr.push(age);
+        nameArr.push(obj.name);
+    })
+
+    let oldest = 0;
+    let i = 0;
+    let oldestIndex = 0;
+
+    for (i = 0; i < ageArr.length; i++) {
+        let currentAge = ageArr[i];
+        if (currentAge >= oldest) {
+            oldestIndex = i;
+            oldest = currentAge;
+        }
+    }
+
+    return nameArr[i];
 };
 
 // Do not edit below this line
